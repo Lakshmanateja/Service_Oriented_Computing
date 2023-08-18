58aa6ae51aac5530960d152c689037bc6e77cbfc
Lakshmana Teja Kapuganti <76482841+Lakshmanateja@users.noreply.github.com>
2023-08-18 23:39:41 +0530

    Add files via upload

---
+++ Lab Assignment-1/E_Ticketing.java
@@ -0,0 +1,48 @@
+import javax.xml.parsers.*; 
+import org.w3c.dom.*;
+import java.io.File;  
+
+
+public class E_Ticketing {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try   
+		{  
+		File file = new File("E-Ticketing XML.xml");  
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();  
+		
+		DocumentBuilder db = dbf.newDocumentBuilder();  
+		Document doc = db.parse(file);  
+		doc.getDocumentElement().normalize();  
+		System.out.println("Root element: " + doc.getDocumentElement().getNodeName());  
+		NodeList nodeList = doc.getElementsByTagName("booking");  
+		
+		for (int i = 0; i < nodeList.getLength(); i++)   
+		{  
+		Node node = nodeList.item(i);  
+		System.out.println("\nNode Name :" + node.getNodeName());  
+		if (node.getNodeType() == Node.ELEMENT_NODE)   
+		{  
+		Element eElement = (Element) node;  
+		System.out.println("Movie Name: "+ eElement.getElementsByTagName("movie").item(0).getTextContent());  
+		System.out.println("Date: "+ eElement.getElementsByTagName("date").item(0).getTextContent());  
+		System.out.println("Time: "+ eElement.getElementsByTagName("time").item(0).getTextContent());  
+		System.out.println("Seats: "+ eElement.getElementsByTagName("seats").item(0).getTextContent());  
+		
+		}  
+		}  
+		}   
+		catch (Exception e)   
+		{  
+		e.printStackTrace();  
+		}  
+		 
+
+	}
+
+}
